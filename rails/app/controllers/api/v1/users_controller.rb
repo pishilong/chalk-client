ec4fe7e882a18a443e65d795f73bc714966d71a7
xhh <xhh@xhh.me>
2013-07-19 15:23:36 +0800

    Separate chalk-ng to two projects: chalk-client and chalk-server

--- rails/app/controllers/api/v1/users_controller.rb
+++
@@ -1,8 +0,0 @@
-module Api::V1
-  class UsersController < ApplicationController
-    def index
-      @users = User.page(params[:page])
-      respond_with @users
-    end
-  end
-end
--- rails/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Chalk::Application.config.secret_token = '5930c9fa30662aa8f7e053e7760eb7cf9d65aeed8697ee42d2531536bf80f47b7e6b5ff10914db9ee3ff107eea9e5564bb00b1e7614f980bfaaf39e162e4ecae'
