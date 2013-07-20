6404f50969b084585d1396950b2c661afe386e6a
xhh <xhh@xhh.me>
2013-07-20 10:25:03 +0800

    Correct angular-placeholders bower-dependency

--- build.config.js
+++ build.config.js
@@ -51,7 +51,7 @@ module.exports = {
     js: [
       'vendor/angular/angular.js',
       'vendor/angular-bootstrap/ui-bootstrap-tpls.min.js',
-      'vendor/angular-placeholders/angular-placeholders.js',
+      'vendor/bower-angular-placeholders/angular-placeholders.js',
       'vendor/angular-ui-router/release/angular-ui-router.js',
       'vendor/angular-ui-utils/modules/route/route.js',
       'vendor/lodash/dist/lodash.js',
